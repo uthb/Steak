0c3d2439e6f55db89135d6d2ca30b4ed5699a151
uthb <czekajroblox@gmail.com>
2025-04-29 17:04:33 +0200

    we

--- api/is-holiday.js
+++ api/is-holiday.js
@@ -1,15 +1,11 @@
-const express = require('express');
 const holidays = require('poland-public-holidays');
-const app = express();
 
-app.get('/is-holiday', (req, res) => {
-    const today = new Date();
-    const result = holidays.IsHoliday(today);
+export default function handler(req, res) {
+  const today = new Date();
+  const result = holidays.IsHoliday(today);
 
-    res.json({
-        isHoliday: !!result,
-        name: result ? result.Name : ''
-    });
-});
-
-app.listen(3000, () => console.log('Server running on port 3000'));
+  res.status(200).json({
+    isHoliday: !!result,
+    name: result ? result.Name : ''
+  });
+}
